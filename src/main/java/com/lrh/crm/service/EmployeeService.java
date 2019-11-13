4cbbecd35233b5839564cd86c72d141375661a04
lrhya <1262946752@qq.com>
2019-11-13 11:22:52 +0800

    pageHelper分页查询功能编写完成

---
+++ src/main/java/com/lrh/crm/service/EmployeeService.java
@@ -0,0 +1,24 @@
+package com.lrh.crm.service;
+
+import com.lrh.crm.bean.Employee;
+import com.lrh.crm.dao.EmployeeMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author lrhya
+ * @version 1.0
+ * @date 2019/11/13 10:41
+ */
+@Service
+public class EmployeeService {
+
+    @Autowired
+    EmployeeMapper employeeMapper;
+
+    public List<Employee> getAll() {
+        return employeeMapper.selectByExample(null);
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/list.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Chris
+  Date: 2019/11/13
+  Time: 10:38
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>员工列表</title>
+</head>
+
+<body>
+
+</body>
+</html>
