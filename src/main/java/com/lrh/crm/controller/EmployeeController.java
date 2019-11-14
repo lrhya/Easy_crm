bdaaba7654b558ffb6c3b3ed4cf08499f149ccc1
lrhya <1262946752@qq.com>
2019-11-14 16:40:26 +0800

    以json形式返回数据

--- src/main/java/com/lrh/crm/controller/EmployeeController.java
+++ src/main/java/com/lrh/crm/controller/EmployeeController.java
@@ -1,20 +1,18 @@
-
 package com.lrh.crm.controller;
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 
 import com.lrh.crm.bean.Employee;
+import com.lrh.crm.bean.Msg;
 import com.lrh.crm.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
-
-
 
 /**
  * @author lrhya
@@ -23,15 +21,34 @@ import java.util.List;
  * 处理员工的增删改查操作
  */
 
-
 @Controller
 public class EmployeeController {
-
 
     @Autowired
     EmployeeService employeeService;
 
+    /**
+     * 需导入jackson包。
+     * @param pn
+     * @return
+     */
     @RequestMapping("/emps")
+    @ResponseBody
+    public Msg getEmpsWithJson(@RequestParam(value = "pn", defaultValue = "1") Integer pn) {
+        // 引入PageHelper分页插件
+        // 在查询之前只需要调用，传入页码，以及每页的大小
+        PageHelper.startPage(pn, 5);
+        // startPage后面紧跟的这个查询就是一个分页查询
+        List<Employee> emps = employeeService.getAll();
+        // 使用pageInfo包装查询后的结果，只需要将pageInfo交给页面就行了。
+        // 封装了详细的分页信息,包括有我们查询出来的数据，传入连续显示的页数
+        PageInfo page = new PageInfo(emps, 5);
+        return Msg.success().add("pageInfo", page);
+    }
+
+/*
+      返回界面数据，淘汰，应返回json数据，可扩展性高
+  //  @RequestMapping("/emps")
     public String getEmps(@RequestParam(value = "pn", defaultValue = "1") Integer pn, Model model){
         // 引入PageHelper分页插件
         // 在查询之前只需要调用，传入页码，以及每页的大小
@@ -44,6 +61,7 @@ public class EmployeeController {
         model.addAttribute("pageInfo", page);
         return "list";
     }
+*/
 
 }
 
--- src/main/java/com/lrh/crm/test/MvcTest.java
+++ src/main/java/com/lrh/crm/test/MvcTest.java
@@ -18,7 +18,6 @@ import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
 
-
 /**
  * @author lrhya
  * @version 1.0
